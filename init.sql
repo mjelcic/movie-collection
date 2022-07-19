15c72d25ddca2285a487da6fc7219294e8a1e2b5
marko.jelcic84 <marko.jelcic84@gmail.com>
2022-07-19 17:31:21 +0200

    Fixing init.sql

--- init.sql
+++ init.sql
@@ -1,2 +1,5 @@
+CREATE DATABASE IF NOT EXISTS movies;
+USE movies;
+
 INSERT IGNORE INTO `user` (`id`, `username`, `password`, `email`, `is_active`, `roles`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
 (1, 'admin', '$2y$13$7W.JDI5BPFtwS.jrIiOG/evgvoPHvYfNqQNfzYy4fDqfoU8RL47YS', 'user@gmail.com', 1, 'ROLE_USER', '2022-07-19 00:45:55', NULL, 1, NULL);
